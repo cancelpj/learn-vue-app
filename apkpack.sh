9b2dec57f58ddcefd0bd01e04453bc68850993ec
pengjun <9997452@qq.com>
2020-06-13 13:45:07 +0800

    更新一键打包脚本。

--- apkpack.sh
+++ apkpack.sh
@@ -1,9 +1,12 @@
+###
+ # @Author: 彭俊
+ # @Date: 2019-11-08 14:46:10
+ # @LastEditTime: 2020-06-13 12:30:16
+ # @LastEditors: 彭俊
+ # @Description: 将本脚本放在 PDA 项目根目录下，用于自动打包 apk（本地需要有 Android Studio 打包环境）
+ # @FilePath: /SP_WEB_iview/Users/pengjun/vscode-workspace/vue/learn-vue-app/apkpack.sh
+### 
 #!/bin/sh
-#############################################
-# 将本脚本放在 PDA 项目根目录下，用于自动打包 apk
-#
-# 作者：彭俊 2019-11-08
-#############################################
 
 # 保存当前路径到变量mydir
 # mydir="${0%/*}"
@@ -16,14 +19,14 @@ date
 echo ----------------------------------------
 npm run build
 
-cd /Users/pengjun/vscode-workspace/android/sp3.0_New
+cd /Users/pengjun/vscode-workspace/android/Android_H5_SP3
 rm -rf ./app/src/assets
 cp -R $mydir/dist ./app/src/assets
 echo
-# read -p "请输入打包APK的版本号（不加V）:" VER
-# echo
-# sed -i '' 's/"X.XX"/"'$VER'"/g' ./app/src/main/java/cs/qingjing/spmes/activitys/HomeActivity.java
-# ./gradlew assembleDebug
-# sed -i '' 's/"'$VER'"/"X.XX"/g' ./app/src/main/java/cs/qingjing/spmes/activitys/HomeActivity.java
-# mv ./app/build/outputs/apk/debug/app-debug.apk /Users/pengjun/Downloads/V$VER.apk
+read -p "请输入打包APK的版本号（不加V）:" VER
+echo
+sed -i '' 's/"XXXXX"/"'$VER'"/g' ./app/src/main/java/com/spmes/h5app/activitys/HomeActivity.java
+./gradlew assembleDebug
+sed -i '' 's/"'$VER'"/"XXXXX"/g' ./app/src/main/java/com/spmes/h5app/activitys/HomeActivity.java
+mv ./app/build/outputs/apk/debug/app-debug.apk /Users/pengjun/Downloads/V$VER.apk
 cd $mydir
